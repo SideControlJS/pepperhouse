fc65aae18c8eb2812c79c2121ce334b7dcbc18e5
Jesse <jesse.lare@gmail.com>
2024-05-25 15:21:09 -0400

    setting up App.js

--- pepperhouse-coffee/src/App.js
+++ pepperhouse-coffee/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ProductPage from './components/ProductPage';
+import CartPage from './components/CartPage';
+import CheckoutPage from './components/CheckoutPage';
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/product/:id" component={ProductPage} />
+          <Route path="/cart" component={CartPage} />
+          <Route path="/checkout" component={CheckoutPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
