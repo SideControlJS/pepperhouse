9897e7de6883c19f8f70bd82bc29396e74db043d
Jesse <jesse.lare@gmail.com>
2024-05-26 14:56:01 -0400

    adding CSS

---
+++ pepperhouse-coffee/src/components/CartPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CartPage = () => {
+  // cart functionality not implemented, yet!
+  return (
+    <div className="cart-page">
+      <h1>Your Cart</h1>
+      <p>Cart functionality coming soon...</p>
+    </div>
+  );
+};
+
+export default CartPage;
---
+++ pepperhouse-coffee/src/components/CheckoutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CheckoutPage = () => {
+  // For simplicity, we're not implementing the full checkout functionality here.
+  return (
+    <div className="checkout-page">
+      <h1>Checkout</h1>
+      <p>Checkout functionality coming soon...</p>
+    </div>
+  );
+};
+
+export default CheckoutPage;
